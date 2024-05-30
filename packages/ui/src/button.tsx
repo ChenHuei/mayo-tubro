d83e71a6f30fec856582d4c7cb84f3077110d2da
LeoChen <leo_chen@mayohr.com>
2024-05-30 13:10:43 +0800

    add button in ui repo

---
+++ packages/ui/src/button.tsx
@@ -0,0 +1,18 @@
+import { type ReactNode, type ButtonHTMLAttributes } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	children: ReactNode;
+}
+
+export function Button(props: ButtonProps): JSX.Element {
+	const { className, children, ...otherProps } = props;
+
+	return (
+		<button
+			className={`ui-p-4 ui-border ui-border-[#eee] ui-rounded ui-cursor-pointer ui-z-10 ${className}`}
+			type="button"
+			{...otherProps}>
+			{children}
+		</button>
+	);
+}
