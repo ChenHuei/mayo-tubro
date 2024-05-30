79d58858b1a80bd318705cc80eacbb49529c5d50
LeoChen <leo_chen@mayohr.com>
2024-05-30 13:34:13 +0800

    add payroll app

---
+++ apps/payroll/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { type ReactNode } from 'react';
+import { Button } from '@repo/ui/button';
+
+export default function Home(): ReactNode {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<Button
+				onClick={() => {
+					console.log('payroll');
+				}}>
+				payroll
+			</Button>
+		</main>
+	);
+}
