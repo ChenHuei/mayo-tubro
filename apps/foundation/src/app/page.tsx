19c5c445b96852c3ac081f2a87c2f78da1116e32
LeoChen <leo_chen@mayohr.com>
2024-05-30 13:54:51 +0800

    add foundation app

---
+++ apps/foundation/src/app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { type ReactNode } from 'react';
+import { Button } from '@repo/ui/button';
+
+export default function Home(): ReactNode {
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<Button
+				onClick={() => {
+					console.log('foundation');
+				}}>
+				Foundation
+			</Button>
+		</main>
+	);
+}
